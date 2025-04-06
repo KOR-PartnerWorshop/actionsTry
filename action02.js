5de725476645b8fccd6b9aa09fd6798d6fa23fc9
son7211 <son7211@github.com>
2025-04-06 16:28:04 +0900

    action.yml 및 action02.js에서 issue_number와 owner, repo 변수를 동적으로 처리하도록 수정

--- action02.js
+++ action02.js
@@ -7,10 +7,12 @@ const octokit = new Octokit({
     baseUrl: 'https://api.github.com'
     });
 
+// GitHub 컨텍스트에서 현재 저장소 정보 가져오기
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'exceeders',
-    repo: 'action-02',
+    owner: owner,
+    repo: repo,
     issue_number: core.getInput('issue_number'),
     labels: [
       'bug'
